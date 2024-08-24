ba32554911ae3cae0e6ca18cc6fe3f91b884764a
Gianluca Gravina <gianluca.gravina@gmail.com>
2024-08-24 22:33:36 +0400

    first kata

---
+++ DuplicateEncoder/src/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+
+public class Kata
+{
+    public static void Main() {
+        DuplicateEncode("Success");
+    }
+  public static string DuplicateEncode(string word)
+  {
+    char[] arr = word.ToLower().ToCharArray();
+    Dictionary<char,int> keys = new Dictionary<char,int>();
+    char[] result = new char[arr.Length];
+
+    for (var i =0; i<arr.Length; i++) {
+      if (keys.ContainsKey(arr[i])) {
+        result[i] = ')';
+        if(keys[arr[i]]>=0) {
+            result[keys[arr[i]]] = ')';
+            keys[arr[i]]=-1;
+        }
+      } else {
+        keys.Add(arr[i],i);
+        result[i] = '(';
+      }
+    }
+    word = new string(result);
+    return word;
+  }
+}
---
+++ DuplicateEncoder/test/UnitTest1.cs
@@ -0,0 +1,13 @@
+
+  [TestFixture]
+  public class KataTests
+  {
+    [Test]
+    public void BasicTests()
+    {
+      Assert.AreEqual("(((", Kata.DuplicateEncode("din"));
+      Assert.AreEqual("()()()", Kata.DuplicateEncode("recede"));
+      Assert.AreEqual(")())())", Kata.DuplicateEncode("Success"), "should ignore case");
+      Assert.AreEqual("))((", Kata.DuplicateEncode("(( @"));
+    }
+  }
