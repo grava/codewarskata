1ab98e07bbd76725e94fca3ae38d2c7dc7763705
Gianluca Gravina <gianluca.gravina@gmail.com>
2024-08-25 13:42:57 +0400

    StringEndsWith

---
+++ Katas/src/StringEndsWith.cs
@@ -0,0 +1,9 @@
+namespace Kata
+{
+    public static class StringEndsWith
+    {
+        public static bool Solution(string str, string ending) {
+            return str.EndsWith(ending);
+        }
+    }
+}
