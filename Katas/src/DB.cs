fa7f2cddac1069eb6da602c40ae1289ca99ddbc3
Gianluca Gravina <gianluca.gravina@gmail.com>
2024-08-26 20:54:32 +0400

    resolved issues

---
+++ Katas/src/DB.cs
@@ -0,0 +1,30 @@
+namespace Kata
+{
+    public static class DB
+    {
+        private static Dictionary<string, string> dict = new Dictionary<string, string>{
+              {"english", "Welcome"}
+            , {"czech", "Vitejte"}
+            , {"danish", "Velkomst"}
+            , {"dutch", "Welkom"}
+            , {"estonian", "Tere tulemast"}
+            , {"finnish", "Tervetuloa"}
+            , {"flemish", "Welgekomen"}
+            , {"french", "Bienvenue"}
+            , {"german", "Willkommen"}
+            , {"irish", "Failte"}
+            , {"italian", "Benvenuto"}
+            , {"latvian", "Gaidits"}
+            , {"lithuanian", "Laukiamas"}
+            , {"polish", "Witamy"}
+            , {"spanish", "Bienvenido"}
+            , {"swedish", "Valkommen"}
+            , {"welsh", "Croeso"}
+            };
+
+        public static Dictionary<string, string> Dict
+        {
+            get => dict;
+        }
+    }
+}
--- Katas/src/Program.cs
+++ Katas/src/Program.cs
@@ -2,6 +2,7 @@
     public static class Katas
     {
         public static void Main() {
+            Console.WriteLine("executing ...");
         }
 
     }
